690de1606c354b3f748338d8e7e4b8baf9142f60
Your Name <amart1015@fiu.edu>
2023-10-23 17:16:08 -0400

    initial commit

---
+++ src/PurpleAirWidget.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+const PurpleAirWidget = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://www.purpleair.com/pa.widget.js?key=8CUTD2YBFY5LBJGE&module=AQI&conversion=C0&average=10&layer=standard&container=PurpleAirWidget_186707_module_AQI_conversion_C0_average_10_layer_standard';
+        document.body.appendChild(script);
+
+        return () => {
+            if (document.body.contains(script)) {
+            document.body.removeChild(script);
+            }
+        };
+        }, []);
+    
+
+  return (
+    <div id='PurpleAirWidget_186707_module_AQI_conversion_C0_average_10_layer_standard'>
+      <div className="loading-container">
+        Loading PurpleAir Widget...<space></space>
+        <div className="spinner"></div>
+      </div>
+    </div>
+  );
+}
+
+export default PurpleAirWidget;
