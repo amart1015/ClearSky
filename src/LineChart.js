e680747b37d739309e32c43c0ea2ae4e7bf66784
Your Name <amart1015@fiu.edu>
2024-03-18 18:11:00 -0400

    Add line chart functionality

--- src/LineChart.js
+++ src/LineChart.js
@@ -7,11 +7,11 @@ import "./LineChart.css";
 const LineChartComponent = () => {
   // Set initial fake data for the line chart
   const [chartData, setChartData] = useState({
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Mar 13th", "Mar 14th", "Mar 15th", "Mar 16th", "Mar 17th", "Mar 18th"],
     datasets: [
       {
         label: "AQI",
-        data: [50, 100, 150, 200, 250, 300], // Fake AQI values
+        data: [41, 39, 34, 48, 43, 67], // Fake AQI values
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         backgroundColor: 'rgba(255, 255, 255, 0.5)', 
